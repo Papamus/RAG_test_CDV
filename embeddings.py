51015902f839cbc534741915af8c1da2ab3e0751
Adam Kusiakiewicz <62795338+Papamus@users.noreply.github.com>
2026-06-13 10:46:48 +0200

    embed model

--- embeddings.py
+++ embeddings.py
@@ -14,7 +14,7 @@ class FAISSIndex:
             results.append(self.metadata[idx])
         return results
 
-embed_model_id = "sentence-transformers/distiluse-base-multilingual-cased-v1" # nazwa modelu
+embed_model_id = "sentence-transformers/all-MiniLM-L6-v2" # nazwa modelu
 model_kwargs = {"device": "cpu", "trust_remote_code": True}
 
 def create_index(documents):
